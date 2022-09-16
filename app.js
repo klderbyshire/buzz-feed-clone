d7b7559978280ee0b514a8855fbdbcb7444a8d4e
karen derbyshire <karenderbyshire5@gmail.com>
2022-09-16 20:32:47 +0100

    array of QnA made

---
+++ app.js
@@ -0,0 +1,35 @@
+const questionDisplay = document.querySelector('#questions')
+const answerDisplay = document.querySelector('#answer')
+
+const questions = [
+    {
+id: 0,
+text: "Pick a vacation destination: ",
+answers: [
+    {
+       text: "New York",
+       image: "https://unsplash.com/photos/5omwAMDxmkU",
+       alt: "Photo of the Empire State building during daytime",
+       credit: "Oliver Nibett"
+    },
+    {
+        text: "Austin",
+        image: "https://unsplash.com/photos/ztVcGTSD8xw",
+        alt: "Photo of Austin City",
+        credit: "Mitchell Kmetz"
+    },
+    {
+        text: "Portland",
+        image: "https://unsplash.com/photos/6iFihNnzqyo",
+        alt: "High rise buildings",
+        credit: "Elena Kuchko"   
+    },
+    {
+        text: "New Orleans",
+        image: "https://unsplash.com/photos/jQwv5FnpksM",
+        alt: "Road with people and house",
+        credit: "João Francisco"
+    }
+]
+    },
+    
